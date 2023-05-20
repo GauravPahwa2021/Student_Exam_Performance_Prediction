4edfd159279f5f2d14bf4d39a1e4c106e6fb739e
Gaurav Pahwa <2020uce1603@mnit.ac.in>
2023-05-20 23:42:02 +0530

    model trainer and utils

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -3,7 +3,7 @@ import sys
 
 from src.exception_handler import CustomException
 from src.logger import logging
-from src.utils import save_object
+from src.utils import evaluate_models
 
 from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
@@ -29,6 +29,25 @@ class ModelTrainer:
 
     def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
         try:
-            pass
+            logging.info("Split train and test input data")
+
+            X_train,y_train,X_test,y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = {
+                "Random Forest" : RandomForestRegressor(),
+                "Decision Tree" : DecisionTreeRegressor(),
+                "XGBoost" : XGBRegressor(),
+                "CatBoost" : CatBoostRegressor(verbose = False),
+                "AdaBoost" : AdaBoostRegressor(),
+                "Gradient Boost" : GradientBoostingRegressor(),
+                "Linear Regression" : LinearRegression(),
+                "KNearestNeighbors" : KNeighborsRegressor()
+            }
+
+            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
         except Exception as e:
             raise CustomException(e,sys)
