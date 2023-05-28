25d2ad0de94ba7b0092d547f623a3b6a36f00952
Gaurav Pahwa <2020uce1603@mnit.ac.in>
2023-05-28 14:43:38 +0530

    template and app

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask,render_template,request
+from flask_cors import cross_origin,CORS
 
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 from src.logger import logging
@@ -17,7 +17,7 @@ def home_page():
 @cross_origin()
 def predict_datapoint():
     if request.method == 'GET':
-        return render_template('home.html')
+        return render_template('index.html')
     else:
         data = CustomData(
             gender = request.form.get('gender'),
@@ -39,28 +39,7 @@ def predict_datapoint():
         results = predict_pipeline.predict(predict_df)
 
         logging.info("After Prediction")
-        return render_template('home.html',results=results[0])
+        return render_template('index.html',results=results[0])
     
-@app.route('/predictAPI',methods=['POST'])
-@cross_origin()
-def predict_api():
-    if request.method=='POST':
-        data = CustomData(
-            gender = request.json['gender'],
-            race_ethnicity = request.json['ethnicity'],
-            parental_level_of_education = request.json['parental_level_of_education'],
-            lunch = request.json['lunch'],
-            test_preparation_course = request.json['test_preparation_course'],
-            reading_score = float(request.json['writing_score']),
-            writing_score = float(request.json['reading_score'])
-        )
-
-        pred_df = data.get_data_as_data_frame()
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-
-        dct = {'math_score':round(results[0],2)}
-        return jsonify(dct)
-
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8000)  
+    app.run(host="0.0.0.0",debug=True,port=8000)  
